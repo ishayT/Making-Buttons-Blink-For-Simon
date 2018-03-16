1ce232d373501cb79e1820ba2853f875cb1ae6a3
Ishay <ishay@Ishays-Mac.local>
2018-03-16 13:51:52 +0200

    Initial Commit

---
+++ Making Buttons Blink For Simon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Making Buttons Blink For Simon
+//
+//  Created by Ishay on 3/16/18.
+//  Copyright © 2018 Ishay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
