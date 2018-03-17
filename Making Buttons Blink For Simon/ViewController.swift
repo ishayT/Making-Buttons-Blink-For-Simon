1424ff5df7f4b16da53c75dea9ca53e80ea637f9
Ishay <ishay@Ishays-Mac.local>
2018-03-17 11:20:43 +0200

    code isn't working for the moment but there is a lot of good code in there

--- Making Buttons Blink For Simon/ViewController.swift
+++ Making Buttons Blink For Simon/ViewController.swift
@@ -9,17 +9,135 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var gameTimer: Timer!
+    
+    @objc let arrayOfnumbers : [Int] = [2,4,2,3,3,3,1,2,4]
+    
+    var index : Int = Int()
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var shadowGiver: UIButton!
+    
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    
+    
+    @objc var arrayOfButtons : [UIButton] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        arrayOfButtons = [button1, button2, button3, button4]
+        
+        index = 0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func playButtons(_ sender: UIButton) {
+        for index in 1...4 {
+            if index == sender.tag {
+                
+                makeButtonFlash(btn: sender)
+            }
+        }
     }
-
-
+    
+    
+    @IBAction func givingShadowForTHeButton(_ sender: UIButton) {
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(runTimedCode), userInfo: nil, repeats: true)
+        //var currentButton = UIButton()
+        //        for index in 0...arrayOfnumbers.count - 1{
+        //
+        //            currentButton = (arrayOfButtons[arrayOfnumbers[index] - 1])
+        //
+        ////            gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(@objc makeButtonFlash(putButtonHere: currentButton)), userInfo: nil, repeats: true)
+        //
+        //            DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
+        //                DispatchQueue.main.async {
+        //                    makeButtonFlash(btn: <#T##UIButton#>)
+        //                }
+        //            }
+        //        }
+        //        var index = 0
+        //        while index < arrayOfnumbers.count {
+        //            makeButtonFlash(btn: arrayOfButtons[arrayOfnumbers[index] - 1])
+        //
+        //            DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
+        //                DispatchQueue.main.async {
+        //                    self.makeButtonFlash(btn: self.arrayOfButtons[self.arrayOfnumbers[index] - 1])
+        //                }
+        //                index += 1
+        //            }
+        //        }
+        
+        
+        //            UIView.animate(withDuration: 1.0, delay: 1.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+        //
+        //
+        //
+        //            }, completion: true)
+        //        var index = 0
+        
+        //        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(callingButtons(index:)), userInfo: nil, repeats: true)
+        //        callingButtons(index: 0)
+    }
+    
+    @ objc func callingButtons (index: Int){
+        //        DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async {
+        //            while index < self.arrayOfnumbers.count - 1{
+        //                DispatchQueue.main.async {
+        //                    UIView.animate(withDuration: 1.0, delay: 1.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+        //                        self.makeButtonFlash(btn: self.arrayOfButtons[self.arrayOfnumbers[index] - 1])
+        //
+        //                    }, completion: { (true) in
+        //                        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+        //                            if index < self.arrayOfnumbers.count - 1 {
+        //                                self.callingButtons(index: index + 1)
+        //
+        //                            }
+        //                        })
+        //                    })
+        //                }
+        //            }
+        //        }
+        if index < self.arrayOfnumbers.count - 1 {
+            
+            UIView.animate(withDuration: 1.0, delay: 1.0, options: UIViewAnimationOptions.curveEaseOut, animations: {self.makeButtonFlash(btn: self.arrayOfButtons[self.arrayOfnumbers[index] - 1]))}
+                
+                
+            }, completion: { (true) in
+                index += 1 })
+            
+        }
+       
+        
+        
+        func makeButtonFlash(btn: UIButton) {
+            btn.layer.shadowColor = UIColor.white.cgColor
+            btn.layer.shadowRadius = 10
+            btn.layer.shadowOffset = CGSize(width: 0, height: 0)
+            btn.flash()
+        }
+        
+    }
 }
 
+extension UIButton {
+    func flash() {
+        
+        let flash = CABasicAnimation(keyPath: "shadowOpacity")
+        flash.duration = 0.8
+        flash.fromValue = 1
+        flash.toValue = 0
+        flash.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        flash.autoreverses = false
+        flash.repeatCount = 1
+        
+        layer.add(flash, forKey: nil)
+    }
+    
+}
